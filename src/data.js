284e121f6dfae6b48119601eecfafb5a591a92aa
Song Aiden Ko <81278660+WindfulSoul@users.noreply.github.com>
2021-12-10 20:47:31 -0800

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'Jane Whittaker',
+      job: 'Web Designer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      text:
+        "It is my pleasure to conduct empathy-driven research and usability tests to provide the best possible solutions and design ideas that fulfill users' requests.",
+    },
+    {
+      id: 2,
+      name: 'Kate Bishov',
+      job: 'UI/UX Designer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+      text:
+        'I am a UI/UX designer specializing in creating information architecture and interactive designs to help people have the best experience on any mobile app or web application.',
+    },
+    {
+      id: 3,
+      name: 'Clint Timhorn',
+      job: 'Software Developer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      text:
+        'In addition to design, I have worked as a software developer with rich knowledge in programming languages such as HTML, CSS, Javascript, Bootstrap, React.Js, and Node.Js.',
+    },
+    {
+      id: 4,
+      name: 'John Henson',
+      job: 'Software Engineer',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      text:
+        'Due to my ability to work both as a designer and a developer, I have been able to perform as a communication bridge between the design and development departments. ',
+    },
+  ];
+  
+  export default reviews;
